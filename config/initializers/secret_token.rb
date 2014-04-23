2680357913299b709383b22a6ced9da950656d40
Kym McInerney <kym@holodigm.com.au>
2014-04-23 20:29:49 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bingbong::Application.config.secret_token = 'a82434c5b65635fce9ebe06217772eb9164c08dc362e7b1312ac94b4f818e3a9cb9364a00f361ba5427e1af2924f0af1609313e16f6bb7b5a149c0b6e40759ef'
